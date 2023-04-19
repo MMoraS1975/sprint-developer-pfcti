e41600cd90a82de5ebb85c1f70906808306cf6eb
MMoraS1975 <131214530+MMoraS1975@users.noreply.github.com>
2023-04-18 18:06:34 -0600

    Primera versión

---
+++ src/main/java/com/pfcti/sprintdevpfcti/SprintDeveloperPfctiApplication.java
@@ -0,0 +1,13 @@
+package com.pfcti.sprintdevpfcti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintDeveloperPfctiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintDeveloperPfctiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfcti/sprintdevpfcti/SprintDeveloperPfctiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfcti.sprintdevpfcti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintDeveloperPfctiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
