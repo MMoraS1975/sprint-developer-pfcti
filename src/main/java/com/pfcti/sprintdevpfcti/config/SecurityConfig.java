f21a3010f5a40d5f373656eaf7297067a45646b4
MMoraS1975 <131214530+MMoraS1975@users.noreply.github.com>
2023-05-02 17:50:04 -0600

    Cambios dia tres

--- src/main/java/com/pfcti/sprintdevpfcti/config/SecurityConfig.java
+++ src/main/java/com/pfcti/sprintdevpfcti/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.http.HttpMethod;
 
@@ -14,26 +11,30 @@ import org.springframework.http.HttpMethod;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withUsername("user")
-                .password("{noop}userPass").roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin").password("{noop}adminPass")
-                .roles("USER", "ADMIN")
-                .build());
-        return manager;
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withUsername("user")
+//                .password("{noop}userPass").roles("USER")
+//                .build());
+//        manager.createUser(User.withUsername("admin").password("{noop}adminPass")
+//                .roles("USER", "ADMIN")
+//                .build());
+//        return manager;
+//    }
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .requestMatchers(HttpMethod.DELETE)
-                .hasRole("ADMIN").requestMatchers("/v1/api/cliente/**", "/v1/api/employee/**").hasAnyRole("USER", "ADMIN").requestMatchers("v1/api/login/**")
-                .anonymous().anyRequest().authenticated().and().httpBasic();
+        http.csrf().disable().authorizeRequests().requestMatchers(HttpMethod.DELETE).
+                hasRole("ADMIN").requestMatchers("/v1/api/cliente/**", "/v1/api/employee/**").
+                hasAnyRole("USER", "ADMIN").
+                requestMatchers("v1/api/login/**").
+                anonymous().anyRequest().
+                authenticated().
+                and().
+                oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
         return http.build();
     }
 }
